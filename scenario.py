8188c2a862b5527c236e60de60f7396f85c7b1d3
Danila Valko <d.v.valko@gmail.com>
2024-03-07 14:07:11 +0100

    s

---
+++ scenario.py
@@ -0,0 +1,37 @@
+"""
+This file contains the mosaik scenario. To start the simulation, just run this
+script from the command line::
+
+    $ python scenario.py
+
+"""
+import os
+import asyncio
+import mango
+
+def check_file_descriptors():
+    if os.name == 'posix':
+        try:
+            descs = os.listdir(os.path.join("/proc", str(os.getpid()), "fd"))
+            print(f"file descriptors in use: {len(descs)}")
+        except:
+            pass
+
+class Agent(mango.Agent):
+    def __init__(self, container, **params):
+        super().__init__(container)
+
+async def _create_container(host, port):
+        return await mango.create_container(addr=(host, port))
+    
+async def _create_agent(container, **params):
+        agent = Agent(container, **params)
+        return agent
+
+host = '0.0.0.0'
+port = 5678
+loop = asyncio.get_event_loop()
+container = loop.run_until_complete(_create_container(host, port))
+agent = loop.run_until_complete(_create_agent(container))
+
+check_file_descriptors()
